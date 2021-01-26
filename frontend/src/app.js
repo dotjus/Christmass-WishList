41a04044e85e032c2559a9781a7791fa11a4f7a2
Justin Ellis <dotjus@gmail.com>
2021-01-26 15:43:24 +1000

    Created front end skeleton for a single christmass gift post.

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import Form from './components/form/form.js'
+import Posts from './components/posts/posts.js'
 
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+import pud from './images/pud.jpeg'
 const App = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    )
+        <Container maxWidth="lg">
+          <AppBar position="static" color="inherit">
+            <Typography variant="h2" align="center">pud</Typography>
+            <img src={pud} alt="pud" />
+          </AppBar>
+          <Grow in>
+            <Container>
+                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form />
+                    </Grid>
+                </Grid>
+            </Container>
+            </Grow>
+        </Container>
+      )
 }
 
 export default App
---
+++ frontend/src/components/posts/posts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Post from './post/post'
+
+const Posts = () => {
+    return (
+        <>
+            <h1>POSTS</h1>
+            <Post />
+            <Post />
+            <Post />
+
+        </>
+    )
+}
+
+export default Posts
